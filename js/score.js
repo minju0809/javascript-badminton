94e8201d97ad496513785cdd09701a4d9c83794d
minju0809 <loverminju88@naver.com>
2022-10-26 17:30:04 +0900

    score 페이지 nan 해결

--- js/score.js
+++ js/score.js
@@ -1,9 +1,9 @@
-// 이전 버튼 // 
+// 이전 버튼 //
 const prevBtn = document.querySelector(".prevBtn");
 
 prevBtn.onclick = () => {
   history.back();
-}
+};
 // prevBtn.addEventListener("click", () => {
 //   history.back();
 // })
@@ -11,6 +11,10 @@ prevBtn.onclick = () => {
 // SCORE //
 let left = document.querySelector("#leftScore");
 let right = document.querySelector("#rightScore");
+let leftnumber = left.innerHTML;
+let rightnumber = left.innerHTML;
+
+// 더블클릭 확인을 위함
 let leftClickCheck = 0;
 let rightClickCheck = 0;
 
@@ -18,35 +22,39 @@ function handleScore(direction) {
   // console.log("direction", direction) // left, right 중 어느쪽 눌렸는지 확인
   if (direction == "left") {
     leftClickCheck++;
+    console.log("1", left);
+
     let timer = setTimeout(() => {
+      // 클릭
       // console.log("reset") // timer reset 확인
       if (leftClickCheck == 1) {
-        left.innerHTML = ++leftScore;
+        left.innerHTML = ++leftnumber;
       }
       leftClickCheck = 0;
     }, 200);
 
+    // 더블클릭
     if (leftClickCheck == 2) {
-      // console.log("left double click"); 
-      clearTimeout(timer)
-      if (leftScore > 0) {
-        left.innerHTML = --leftScore;
+      console.log("left double click");
+      clearTimeout(timer);
+      if (leftnumber > 0) {
+        left.innerHTML = --leftnumber;
       }
       leftClickCheck = 0;
     }
   } else if (direction == "right") {
     rightClickCheck++;
     let timer = setTimeout(() => {
-      if(rightClickCheck == 1) {
-        right.innerHTML = ++rightScore;
+      if (rightClickCheck == 1) {
+        right.innerHTML = ++rightnumber;
       }
       rightClickCheck = 0;
     }, 200);
 
     if (rightClickCheck == 2) {
-      clearTimeout(timer)
-      if (rightScore > 0) {
-        right.innerHTML = --rightScore;
+      clearTimeout(timer);
+      if (rightnumber > 0) {
+        right.innerHTML = --rightnumber;
       }
       rightClickCheck = 0;
     }
